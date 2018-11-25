7f2c60786eddfe4fcc93d9b033b61d27a86ea7d3
mike <mike_4596@hotmail.com>
2018-11-24 23:08:35 -0800

    bulk of css displayed

--- page-submit.php
+++ page-submit.php
@@ -16,24 +16,27 @@ get_header(); ?>
 
             <?php if( is_user_logged_in() && current_user_can( 'edit_posts')): ?>
                 <div class="quote-submission-wrapper">
+                <header class="entry-header">
+                <?php the_title('<h1 class="entry-title">','</h1>'); ?>
+            </header>
                     <form name="quoteForm" id="quote-submission-form">
-                        <div>
+                        <div class="author">
                             <label for=""> Author of Quote</label>
                             <input type="text" name="quote_author" id="quote-author">
                         </div>
-                        <div>
+                        <div class="content">
                             <label for="quote-content">Quote</label>
                             <textarea name="quote_content" id="quote-content" rows="3" cols="20"></textarea>
                         </div>
-                        <div>
+                        <div class="source">
                             <label for="">Where did you find this quote? (e.g. book name)</label>
                             <input type="text" name="quote_source" id="quote-source">
                         </div>
-                        <div>
+                        <div class="url">
                             <label for="">Provide the URL of the quote source, if available.</label>
                             <input type="url" name="quote_source_url" id="quote-source-url">
                         </div>
-                        <input type="submit" value="Submit Quote">
+                        <input class="submit-quote-button" type="submit" value="Submit Quote">
                     </form>
                 </div>
 
@@ -43,7 +46,7 @@ get_header(); ?>
                 <?php the_title('<h1 class="entry-title">','</h1>'); ?>
             </header>
                 <p> sorry, please log in to submit a quote.</p>
-                <p><?php echo sprintf( '<a href"%1s">%2s</a>', esc_url( wp_login_url() ), 'click here to login.' );?></p>
+                <a href="http://localhost:3000/quotes-on-dev/wp-login.php">click here to login.</a>
                 </div>
             <?php endif; ?>
         </section
